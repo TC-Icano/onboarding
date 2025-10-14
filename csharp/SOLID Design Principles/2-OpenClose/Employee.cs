4191e26fc5ec4a52b096525989ae84c2f5844187
TC-Icano <icano@tigerconnect.com>
2025-10-14 11:39:46 -0600

    refactor:  Refactor employee classes to follow Open/Closed Principle
    Introduced an abstract Employee base class to enforce a common
    interface for all employee types.

---
+++ csharp/SOLID Design Principles/2-OpenClose/Employee.cs
@@ -0,0 +1,21 @@
+﻿
+namespace _2_OpenClose
+{
+    public abstract class Employee
+    {
+        /// <summary>
+        /// Gets or sets the full name of the employee.
+        /// </summary>
+        public abstract string Fullname { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total number of hours worked.
+        /// </summary>
+        public abstract int HoursWorked { get; set; }
+
+        /// <summary>
+        /// Calculates the monthly salary for an employee.
+        /// </summary>
+        public abstract void CalculateSalaryMonthly();
+    }
+}
--- csharp/SOLID Design Principles/2-OpenClose/EmployeeFullTime.cs
+++ csharp/SOLID Design Principles/2-OpenClose/EmployeeFullTime.cs
@@ -1,14 +1,38 @@
+using _2_OpenClose;
+
 namespace OpenClose
 {
-    public class EmployeeFullTime
+    public class EmployeeFullTime : Employee
     {
-        public string Fullname { get; set; }
-        public int HoursWorked { get; set; }
-
         public EmployeeFullTime(string fullname, int hoursWorked)
         {
             Fullname = fullname;
             HoursWorked = hoursWorked;
-        }  
+        }
+
+        public override string Fullname
+        {
+            get => _fullname;
+            set => _fullname = value;
+        }
+
+        public override int HoursWorked
+        {
+            get => _hoursWorked;
+            set => _hoursWorked = value;
+        }
+
+        /// <summary>
+        /// Calculates and displays the monthly salary of the employee based on their hourly rate and hours worked.
+        /// </summary>
+        public override void CalculateSalaryMonthly()
+        {
+            decimal hourValue = 30000M;
+            decimal salary = hourValue * HoursWorked;
+            Console.WriteLine($"Employee: {Fullname}, Salary: {salary:C1} ");
+        }
+
+        private string _fullname;
+        private int _hoursWorked;
     }
 }
