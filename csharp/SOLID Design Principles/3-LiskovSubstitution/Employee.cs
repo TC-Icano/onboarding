01224f58bd13688712fbe822761a884015014f50
TC-Icano <icano@tigerconnect.com>
2025-10-14 13:19:41 -0600

    refactor:
    Refactor salary calculation logic in Employee hierarchy
    
    Refactored the `Employee` class to make `CalculateSalary` an abstract method, enforcing specific salary calculation logic in derived classes. Updated `EmployeeContractor` and `EmployeeFullTime` to override `CalculateSalary` with their respective rules.
    
    Improved constructor readability in `Employee`, `EmployeeContractor`, and `EmployeeFullTime` by using a multi-line format. Properly initialized the `ExtraHours` property in `Employee`.
    
    Simplified `CalculateSalaryMonthly` in `Program.cs` by leveraging polymorphism, removing type checks for salary calculation.

--- csharp/SOLID Design Principles/3-LiskovSubstitution/Employee.cs
+++ csharp/SOLID Design Principles/3-LiskovSubstitution/Employee.cs
@@ -6,16 +6,21 @@ namespace Liskov
         public int HoursWorked { get; set; }
         public int ExtraHours {get;set;}
 
-        public  Employee(string fullname, int hoursWorked, int ExtraHours)
+        public  Employee(
+            string fullname,
+            int hoursWorked,
+            int extraHours
+        )
         {
             Fullname = fullname;
             HoursWorked = hoursWorked;
-        }  
+            ExtraHours = extraHours;
+        }
 
-        public virtual decimal CalculateSalary (bool IsFullTime)
-        {   
-            decimal hourValue = IsFullTime ? 50 : 40;
-            return hourValue * (HoursWorked + ExtraHours);
-        } 
+        /// <summary>
+        /// Calculates the salary of the employee.
+        /// </summary>
+        /// <returns></returns>
+        public abstract decimal CalculateSalary();
     }
 }
--- csharp/SOLID Design Principles/3-LiskovSubstitution/EmployeeContractor.cs
+++ csharp/SOLID Design Principles/3-LiskovSubstitution/EmployeeContractor.cs
@@ -2,8 +2,22 @@ namespace Liskov
 {
     public class EmployeeContractor : Employee
     {
-        public EmployeeContractor(string fullname, int hoursWorked, int extrahours) : base(fullname, hoursWorked, extrahours)
+        public EmployeeContractor(
+            string fullname,
+            int hoursWorked,
+            int extraHours
+        ) : base(fullname, hoursWorked, extraHours)
         {
+        }
+
+        /// <summary>
+        /// Calculates the total salary based on the standard work hours and extra hours worked.
+        /// </summary>
+        /// <returns>The total salary as a <see cref="decimal"/> value, calculated using the formula:  40 multiplied by the sum
+        /// of <c>HoursWorked</c> and <c>ExtraHours</c>.</returns>
+        public override decimal CalculateSalary()
+        {
+            return 40 * (HoursWorked + ExtraHours);
         }
     }
 }
